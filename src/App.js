67a11d3b1dfbab02ee696ecf7044295ccbafdff9
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-11 13:15:02 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/joseane-ivelise-grigolo-863a1a35/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Joseane Grigolo{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/JoseaneGrigolo/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
