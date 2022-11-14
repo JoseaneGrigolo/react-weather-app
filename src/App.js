bc11bce07a8c7b962b9b95ce67ba35b7efb14995
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-14 10:48:20 -0600

    bootstrap add

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/joseane-ivelise-grigolo-863a1a35/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Joseane Grigolo{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/JoseaneGrigolo/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/joseane-ivelise-grigolo-863a1a35/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joseane Grigolo{" "}
+          </a>
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/JoseaneGrigolo/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
