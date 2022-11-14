560c213363faf6ce4b230f3fc31a8324e3e503b0
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-14 13:33:42 -0600

    api integration

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -74,8 +74,7 @@ export default function Weather() {
     );
   } else {
     const apiKey = "52e34c3750a0a87a4e68575b57b95041";
-    let city = "London";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
