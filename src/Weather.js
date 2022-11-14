201774c6afb00906e59c57899726da38e78c0e01
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-14 12:51:12 -0600

    update weather

---
+++ src/Weather.js
@@ -0,0 +1,55 @@
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Monday 10:00</li>
+        <li>Clear</li>
+      </ul>
+
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Clear"
+              className="float-start"
+            />
+            <div>
+              <span className="temperature float-start">6 </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 46%</li>
+            <li>Wind: 11km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
