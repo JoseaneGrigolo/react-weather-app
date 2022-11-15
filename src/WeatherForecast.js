7213af317bd2b9e19a993c6a80b25385fcb56dd1
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-15 15:59:28 -0600

    error correct

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
 import "./WheaterForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
