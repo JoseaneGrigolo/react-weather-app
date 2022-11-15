500461e29a651b39c80cdd03db839cc8212c90c4
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-15 15:26:27 -0600

    update 1 day forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,8 +1,25 @@
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
+    return day;
+  }
+
   return (
     <div className="WeatherForecastDay">
       {" "}
-      <div className="WeatherForecast-day">{props.data.time}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <div className="WeatherForecast-icon">
         <img
           src={props.data.condition.icon_url}
@@ -11,14 +28,8 @@ export default function WeatherForecastDay(props) {
         />{" "}
       </div>
       <div className="WeatherForecast-temperatures">
-        <span className="WeatherForecast-max">
-          {" "}
-          {Math.round(props.data.temperature.maximum)}°
-        </span>
-        <span className="WeatherForecast-min">
-          {" "}
-          {Math.round(props.data.temperature.minimum)}°
-        </span>
+        <span className="WeatherForecast-max"> {maxTemperature()}°</span>
+        <span className="WeatherForecast-min"> {minTemperature()}°</span>
       </div>
     </div>
   );
